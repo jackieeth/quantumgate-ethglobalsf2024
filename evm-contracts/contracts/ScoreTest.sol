3d2dd7c1f2f21b47056da962aebecfe71229efd1
jackie lee <jackylee0424@users.noreply.github.com>
2024-10-18 23:04:57 -0700

    add check sig helper func

--- evm-contracts/contracts/ScoreTest.sol
+++ evm-contracts/contracts/ScoreTest.sol
@@ -45,5 +45,65 @@ contract ScoreTest is ERC20, Ownable {
         }
     }
 
-    
+    // helper func to return the decimal string representation of value
+    function itoa(uint value) public pure returns (string memory) {
+        // Count the length of the decimal string representation
+        uint length = 1;
+        uint v = value;
+        while ((v /= 10) != 0) {
+            length++;
+        }
+
+        // Allocated enough bytes
+        bytes memory result = new bytes(length);
+
+        // Place each ASCII string character in the string,
+        // right to left
+        while (true) {
+            length--;
+
+            // The ASCII value of the modulo 10 value
+            result[length] = bytes1(uint8(0x30 + (value % 10)));
+
+            value /= 10;
+
+            if (length == 0) {
+                break;
+            }
+        }
+
+        return string(result);
+    }
+
+    // check eth sig
+    function _ecrecover(
+        string memory message,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal pure returns (address) {
+        // Compute the EIP-191 prefixed message
+        bytes memory prefixedMessage = abi.encodePacked(
+            "\x19Ethereum Signed Message:\n",
+            itoa(bytes(message).length),
+            message
+        );
+
+        // Compute the message digest
+        bytes32 digest = keccak256(prefixedMessage);
+
+        // Use the native ecrecover provided by the EVM
+        return ecrecover(digest, v, r, s);
+    }
+
+    // recover address from sig v, r, s
+    function recoverStringFromVRS(
+        string calldata message,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public pure returns (address) {
+        // The v, r and s are included directly within the struct, which is 96 bytes in length
+        return _ecrecover(message, v, r, s);
+    }
 }
